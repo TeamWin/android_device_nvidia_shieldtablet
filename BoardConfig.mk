48958219ed3ac8ad2bcb8d1cade3e1052b1ac908
lj50036 <jl52741@gmail.com>
2015-12-01 23:18:57 -0600

    shieldtablet: Add decryption support
    Change-Id: If23135aa7f2569e9fbc1634334c092dbb6e49b3a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,6 +104,7 @@ BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_BRIGHTNESS_PATH := /sys/class/backlight/pwm-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
+TW_INCLUDE_CRYPTO := true
 
 # MultiROM
 MR_INPUT_TYPE := type_b
