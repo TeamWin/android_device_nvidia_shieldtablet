9b8772627795492f3380d2bf86680c09aada92c9
Aaron Kling <webgeek1234@gmail.com>
2016-05-26 12:06:59 -0500

    Fix omni files

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni.mk
+    $(LOCAL_DIR)/omni_shieldtablet.mk
--- omni_shieldtablet.mk
+++ omni_shieldtablet.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
 
 # Inherit some common OMNI stuff.
-$(call inherit-product, vendor/omni/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 PRODUCT_NAME := omni_shieldtablet
 PRODUCT_DEVICE := shieldtablet
